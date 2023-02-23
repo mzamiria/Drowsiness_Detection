2fbe2eae073469e3115b522e5126e5ee7af141dd
mzamiria <117386509+mzamiria@users.noreply.github.com>
2023-02-23 14:20:44 -0500

    Update Drowsiness_python_model.py

--- app/Drowsiness_python_model.py
+++ app/Drowsiness_python_model.py
@@ -18,17 +18,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.utils import plot_model
 
-from IPython.display import SVG, Image
-from livelossplot import PlotLossesKeras
-from livelossplot.keras import PlotLossesCallback
 import tensorflow as tf
-
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.applications.resnet50 import preprocess_input
-
-# change directory to import utils and read other folders
-os.chdir(r'C:\Users\user\Desktop\Drowsiness')
-
 
 
     
@@ -59,52 +49,32 @@ validation_generator=datagen_validation.flow_from_directory('Images/',
 
 #Create CNN
 model=Sequential()
-model.add(Conv2D(64,(3,3),padding='same',input_shape=(img_size,img_size,1)))
-model.add(BatchNormalization())
+model.add(Conv2D(32,(3,3),padding='same',input_shape=(img_size,img_size,1)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(2,2))
-model.add(Dropout(0.25))
 
-model.add(Conv2D(128,(5,5),padding='same'))
-model.add(BatchNormalization())
+model.add(Conv2D(64,(3,3),padding='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(2,2))
-model.add(Dropout(0.25))
 
-model.add(Conv2D(512,(5,5),padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(2,2))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(512,(5,5),padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(2,2))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(256))
-model.add(BatchNormalization())
+model.add(Conv2D(128,(3,3),padding='same'))
 model.add(Activation('relu'))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(512))
-model.add(BatchNormalization())
+model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dropout(0.25))
 
 model.add(Dense(2, activation='softmax'))
 opt=Adam(learning_rate=0.0005)
-
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
-epochs=7
+epochs=25
 steps_per_epoch=train_generator.n//train_generator.batch_size
 validation_steps=validation_generator.n//validation_generator.batch_size
 
+# saving the weights
 checkpoint=ModelCheckpoint('model_weights.h5', monitor='val_accuracy',
                            save_weights_only=True, mode='max',verbose=1)
 reduce_lr=ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, min_lr=0.00001, mode='auto')
@@ -112,6 +82,8 @@ callbacks=[checkpoint,reduce_lr]
 
 history=model.fit(train_generator, steps_per_epoch=steps_per_epoch, epochs=epochs,
                   validation_data=validation_generator,validation_steps=validation_steps, callbacks=callbacks)
+
+# saving the model
 model_json=model.to_json()
 with open('model.json', 'w') as json_file:
     json_file.write(model_json)
